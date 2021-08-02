6c977e93efd6f98bffa13cb7c60440c09cd1394f
Lisa Wagner <lisawagner.online@gmail.com>
2021-08-01 17:44:56 -0700

    server code updates

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -4,9 +4,7 @@ const commentsResolvers = require("./comments");
 
 module.exports = {
   Post: {
-    likeCount(parent) {
-      return parent.likes.length;
-    },
+    likeCount: (parent) => parent.likes.length,
     commentCount: (parent) => parent.comments.length,
   },
   Query: {
--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -28,7 +28,10 @@ module.exports = {
   Mutation: {
     async createPost(_, { body }, context) {
       const user = checkAuth(context);
-      console.log(user);
+
+      if (body.trim() === "") {
+        throw new Error("Post body must not be empty");
+      }
 
       const newPost = new Post({
         body,
