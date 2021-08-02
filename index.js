79e2f023a0846c619ff217a030d5f60ddece226b
Lisa Wagner <lisawagner.online@gmail.com>
2021-08-02 16:48:09 -0700

    updated port

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers");
 const { MONGODB } = require("./config.js");
 
+const PORT = process.env.port || 5000;
+
 // create server instance
 const server = new ApolloServer({
   typeDefs,
@@ -18,8 +20,11 @@ mongoose
   .then(() => {
     // start server
     console.log("MongoDB Connected");
-    return server.listen({ port: 5000 });
+    return server.listen({ port: ProgressEvent });
   })
   .then((res) => {
     console.log(`Server Running at ${res.url}`);
+  })
+  .catch((err) => {
+    console.error(err);
   });
